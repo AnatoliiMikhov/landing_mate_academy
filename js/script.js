112b3f078479766881fe9531cd0e8b68f53a93b4
Anatolii Mikhov <anatoli.mihov@gmail.com>
2023-12-15 15:26:13 +0100

    added scripts to the top button. the button appears AND disappears when scrolling

---
+++ js/script.js
@@ -0,0 +1,12 @@
+let myButton = document.getElementById( "goup" )
+window.onscroll = function () {
+    if ( window.scrollY >= 720 ) {
+        myButton.style.display = "flex"
+        console.log( "I am here!" )
+    } else {
+        myButton.style.display = "none"
+    }
+}
+myButton.onclick = function () {
+    window.scrollTo( 0, 0 )
+}
